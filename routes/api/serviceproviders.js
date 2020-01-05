8ec00895ff513c227a7cd6a10431a8ff4d0c25f1
Lakshin Kumar <lakshin.kumar.lk@gmail.com>
2020-01-05 18:01:56 +0530

    Creation
    files created with yarn. start with yarrn run dev. 3k for react 1234 for node server

--- routes/api/serviceproviders.js
+++ routes/api/serviceproviders.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test',(req,res) => res.json({msg:"service Works"})
-);
+router.get('/test', (req, res) => res.json({
+    msg: "service Works"
+}));
 
-module.exports= router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,34 +1,35 @@
 const express = require('express');
-const  mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-
 const users = require('./routes/api/users');
-const serviceproviders = require('./routes/api/serviceproviders');
+const serviceProviders = require('./routes/api/serviceProviders');
+const businessProviders = require('./routes/api/businessProviders');
 
-const app=express();
+const app = express();
 
 //Body parser middleware
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
 
-//dbconfig 
+//dbconfig
 const db = require('./config/keys').mongoURI;
-//connetc to mongo db
+//connect to mongo db
 mongoose
-.connect(db)
-.then(()=> console.log('MongoDB connected'))
-.catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 //use routes
-app.use('/api/users',users);
-app.use('/api/serviceproviders',serviceproviders);
+app.use('/api/users', users);
+app.use('/api/serviceProviders', serviceProviders);
 
+//app.get('/',(req,res) => res.send('hello  world'));
 
+const port = process.env.PORT || 1234;
 
-app.get('/',(req,res) => res.send('hello  world'));
-
-const port =process.env.PORT || 5000;
-
-app.listen(port,() => console.log(`Server running on the port ${port}` ));
-
+app.listen(port, () => console.log(`Server running on the port ${port}`));
